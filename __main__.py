b38bfe3f9e131d7b508a4accfdb9f3f9efad2c9c
rod holland <rholland@structurelabs.com>
2014-07-09 23:14:37 -0700

    0.4.0 use latexmk with xetex to reduce verboisty

--- __main__.py
+++ __main__.py
@@ -1,82 +1,124 @@
-""" This module turns the oncepy package into a program module
+""" This module makes the oncepy package a program module
 
-The module also determines whether the file is a model or
-project file and sets the working directory.
+It determines whether the file is a model or project file
+Start the program from the model directory with
+python -m oncepy modelname.txt
 
-Start program with python -m oncepy modelname
 """
-
+from __future__ import division
+from __future__ import print_function
 import os
 import sys
+import oncepy
 from oncepy import cstart
 from oncepy import cproj
+from oncepy import ccheck
 import oncepy.oconfig as cfg
 
 
-__all__ = ["cstart", "ccheck", "cdict", "ctext", "cpdf", "cproj"]
+__all__ = ["cstart", "cdict", "ctext", "cpdf",
+           "cproj", "ccheck"]
 __version__ = "0.4.0"
 __author__ = 'rholland'
 
 if __name__ == '__main__':
-    #print(sys.argv)
-    sysargv = sys.argv
+
+    #read command line arguments
     cfg.sysargv = sys.argv
     startmod = cstart.ModStart
     startproj = cproj.ProjStart
 
     # check if command line is valid
-    if len(sysargv) < 2:
-        startmod._cmdline()
-    elif sysargv[1] == '-h':
-        startmod._cmdline()
+    if len(cfg.sysargv) < 2:
+        startmod.cmdline(__version__)
+    elif cfg.sysargv[1] == '-h':
+        startmod.cmdline(__version__)
 
-    # set unit file source
-    mfile = os.getcwdb()
-    try:
-        from unitc import *
-        cfg.unitfile = 'model folder'
-    except ImportError:
-        try:
-            os.chdir(os.pardir)
-            from unitc import *
-            cfg.unitfile = 'project folder'
-            os.chdir(mfile)
-        except ImportError:
-            from oncepy.unitc import *
-            cfg.unitfile = 'built-in'
+    # set model file path
+    _mfile = cfg.mfile = cfg.sysargv[1]
+    _mpath = cfg.mpath = os.getcwd()
+    os.chdir(_mpath)
+    #print(_mpath)
 
-    # set PDF style file source
+    # start error and event log
+    _ew = ccheck.ModCheck()
+    _ew.ewrite1()
+
+    # set PDF style file path
     try:
-        f1 = open('once.sty')
+        _f1 = open('once.sty')
+        _f1.close()
         cfg.stylefile = 'model folder'
-    except IOError:
+    except IOError as _e:
         try:
             os.chdir(os.pardir)
-            f1 = open('once.sty')
+            _f1 = open('once.sty')
             cfg.stylefile = 'project folder'
-            os.chdir(mfile)
-        except IOError:
-            f1 = open('once.sty')
-            cfg.unitfile = 'built-in'
+            os.chdir(_mpath)
+        except IOError as _e:
+            os.chdir(oncepy.__path__[0])
+            _f1 = open('once.sty')
+            cfg.stylefile = 'built-in'
+            os.chdir(_mpath)
 
-    # check if the file is a model or project file
-    mfile = ''
-    pfile = ''
-    f1 = open(sysargv[1], 'r')
-    readlines1 = f1.readlines()
-    f1.close()
-    for i1 in readlines1:
+    # look in file to check for a project
+    _f1 = open(cfg.sysargv[1], 'r')
+    _readlines1 = _f1.readlines()
+    _f1.close()
+    for _i1 in _readlines1:
         # check for [p] tag
-        if len(i1.strip()) > 0:
+        if len(_i1.strip()) > 0:
             #print(i1)
-            if i1.split()[0].strip()[0:3] == '[p]':
-                cfg.pfile = sysargv[1]
+            if _i1.split()[0].strip()[0:3] == '[p]':
+                cfg.pfile = cfg.sysargv[1]
                 cfg.ppath = os.getcwd()
-                print("project operations not complete")
-                #startproj()
-                continue
+                print("project operations are not ")
+                #_startproj()
 
-    # if no [p] tag then run model
-    cfg.mfile = sysargv[1]
-    cfg.mpath = os.getcwd()
+    # if no [p] tag in file run as model
     startmod()
+    # clean up
+    if len(cfg.sysargv) > 2 and '-noclean' in cfg.sysargv:
+        pass
+    else:
+            # clean out any auxiliary files
+        rstfile = cfg.mfile.replace('.txt', '.rst')
+        texfile = cfg.mfile.replace('.txt', '.tex')
+        auxfile = cfg.mfile.replace('.txt', '.aux')
+        outfile = cfg.mfile.replace('.txt', '.out')
+        logfile = cfg.mfile.replace('.txt', '.log')
+        texmak1 = cfg.mfile.replace('.txt', '.fls')
+        texmak2 = cfg.mfile.replace('.txt', '.fdb_latexmk')
+
+        try:
+            os.remove(rstfile)
+        except OSError:
+            pass
+        try:
+            os.remove(auxfile)
+        except OSError:
+            pass
+        try:
+            os.remove(texfile)
+        except OSError:
+            pass
+        try:
+            os.remove(outfile)
+        except OSError:
+            pass
+        try:
+            os.remove(logfile)
+        except OSError:
+            pass
+        try:
+            os.remove(texmak1)
+        except OSError:
+            pass
+        try:
+            os.remove(texmak2)
+        except OSError:
+            pass
+
+    # check that error log is closed
+    _ew.ewclose()
+
