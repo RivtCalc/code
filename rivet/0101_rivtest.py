4d6142f282cccac24b307aa3e1dcd77672adde0d
rhholland <rhholland@structurelabs.com>
2019-07-28 03:48:23 -0700

    0728

--- rivet/0101_rivtest.py
+++ rivet/0101_rivtest.py
@@ -1,10 +1,23 @@
 #! python
-# | [rivet] | calculation name | [date] |
+#%% section 1
+i__(f"""| function label [s] |
+    |[rivet]| calculation name |[date] [toc] [pdf] [clean]|
+    
+    The need to distinguish between the various meanings of "frame of
+    reference" has led to a variety of terms. For example, sometimes the type
+    of coordinate system is attached as a modifier, as in Cartesian frame of
+    reference. Sometimes the state of motion asfda asd s fdas sfdfasdfasdf is
+    emphasized, as in rotating frame of reference. Sometimes
+
+    | [figure] | filename | caption  | size | [+] |
+    | [figure] | filename | caption  | size | 
+    | [insert] | textfile  | [i][b] |
+    | [latex]  | α = \integral(x^2,x,a,b) |
+    | [math]   | asciiformula |
+    """)
 
 
-#%%
-r__(
-    f"""| a caption plot [s] | filename [o] | [p] |
+r__(f"""| a caption plot [s] [p] | filename [o] | 
     fig, ax1 = plt.subplots()
     t = np.arange(0.01, 10.0, 0.01)
     s1 = np.exp(t)
@@ -22,73 +35,57 @@ r__(
 
     fig.tight_layout()
     plt.show()
-    """
-)
+    """)
 
-i__(
-    f"""| function label |
-    The need to distinguish between  the various
-    meanings of "frame of reference" has led to a variety of terms. For
-    example, sometimes the type of coordinate system is attached as a
-    modifier, as in Cartesian frame of reference. Sometimes the state of
-    motion asfda asd s fdas sfdfasdfasdf is emphasized, as in rotating frame
-    of reference. Sometimes
+v__(f"""| values abcd |
+    Some text if needed
 
-    | [figure] | filename | caption  | size | [d] |
-    | [figure] | filename | caption  | size | [d] |
-    | [insert] | textfile  | [i][b] |
-    | [latex]  | \alpha = \integral(x^2,x,a,b) |
-    | [math]   | asciiformula |
-    """
-)
+    a11 = 12.23*IN
+    value description
 
-# |newpage|
-
-v__(
-    f"""| values abcd  |
-    a11 = 12*4  #|2, 2|
-    a22 = 12.2
+    a22 = 12.2*LBS
+    value description 
+    
     a33 = 14
-    """
-)
+    description
+    """)
 
-e__(
-    f"""equations  abcd     | ref
+e__(f"""| equations  abcd |
     aa2 = a11*4
-    aa22 = a11*5
-    """
-)
+    equation description | units |2,2|
 
-#%% section 0
-i__(
-    f"""This is a test γ = 2*Σ of the system and this is a further test
-    of the system and another greek letter Γ₂.
+    aa22 = a11*5
+    equation description | units |2,2|
+    """)
+
+#%% section 2
+i__(f"""| some description [s]|
+    This is a test γ = 2*Σ of the system and this is a further test
+    and another greek letter Γ₂.
 
     The need to distinguish between the various meanings of "frame of reference"
     has led to a variety of terms. For example, sometimes the type of coordinate
-
-    the way it transforms to frames considered as related is emphasized as in
+    
+    |[newpage]|
+    
+    The way it transforms to frames considered as related is emphasized as in
     Galilean frame of reference. Sometimes frames are distinguished by the scale of
     their observations, as in macroscopic and microscopic frames of reference.[1]
-    """
-)
+    """)
 
-v__(
-    f"""|some values |
-    gg = 5.4    # height of roof {gam}   |    |
-    hh = 12.2   # height of balcony |   |
-    """
-)
+v__(f"""| some values |
+    gg = 5.4*FT    
+    height of roof 
+    
+    hh = 12.2 
+    height of balcony
+    """)
 
-i__(
-    f"""|text|
-    this is a one liner 4 / {sig}
-    """
-)
+i__(f"""| text |
+    this is a one liner 4 γ
+    """)
 
-e__(
-    """|some equations |
+e__(f"""| some equations |
     xx1 = gg + 4
     xx2 = hh + 10
-    """
-)
+    """)
