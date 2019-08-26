e356a957f4d8fb59d4ee2c837f11104a898fef72
rholland <rholland@structurelabs.com>
2019-08-25 21:30:38 -0700

    0825a

--- rivet/__main__.py
+++ rivet/__main__.py
@@ -11,26 +11,25 @@ documentation are here: https://github.com/r-i-v-e-t .  For an overview of
 
 A **r-i-v-e-t** design is an ASCII text file containing engineering calculations
 in the **r-i-v-e-t** and Python language format. Design files have names of the
-form *ddcc_design_filename.py* where dd and cc are two digit numbers identifying
+form *rddcc_design_filename.py* where dd and cc are two digit numbers identifying
 the division and calculation number respectively. Division numbers apply to
 **r-i-v-e-t**  reports which are organized compilations of calculations.
 
 Designs and their required supporting files must be stored in their respective
 *designs* subfolder. Output calc files are written to the *calcs* directory in
 html and text (utf) format. PDF calcs, if specified, are written to the report
-folder and may be assembled into collated reports. To protect against data loss
-the user must initially set up a project by creating the project folder
-structure using the following naming conventions::
+folder and may be assembled into collated reports. The user must initially set
+up a project by creating the project folder structure using the following
+naming conventions::
 
   Project_Name (chosen by user)
-      |- calcs
-          |- txt
-      |- designs
-          |- figures
+      |- design
           |- scripts
           |- tables
       |- reports
           |- attachments
+          |- figures
+          |- HTML          
           |- temp
 
 The program will display an error and stop if the design file is not located
@@ -43,7 +42,7 @@ file set) from the command line by invoking the rivet package as follows:
 
 .. code:: python
 
-    python rivet ddcc_ design_filename.py  (list_of_files.txt)
+    python rivet rddcc_ design_filename.py  (list_of_files.txt)
 
 """
 
