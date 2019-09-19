205edaded310966fc8caa42e73bded1a2d07c7b8
Rodney Holland <rholland>
2019-09-19 05:36:57 -0700

    0919a

--- rivet/rivet_lib.py
+++ rivet/rivet_lib.py
@@ -39,85 +39,77 @@ import rivet.rivet_utf as _utf
 # from rivet.rivet_html import *
 # from rivet.rivet_pdf import *
 
+
 __version__ = "0.9.1"
 __author__ = "rholland"
-
 if sys.version_info < (3, 7):
     sys.exit("rivet requires Python version 3.7 or later")
 
+# set up folders, files and dictionaries
 global_rivet_dict = {}
-eqnum, sectnum = 0, 0
 
-try: 
-    designfile = main.__file__
-except:
-    designfile = __file__
-_designfile = designfile
-print('design file ', designfile)
+def ad_paths(_settings):
+    _path_dict = {
+    "adpath" : _settings["path"],
+    "adfile" : _ad_set["file"],
+    "rivetpath" : os.path.dirname("rivet.rivet_lib.py"),
+    "projpath" : os.chdir(os.pardir),
+    "reptpath" : os.path.join(_projpath, "reports"),
+    }
+    ad_folders = {
+    "spath" :  os.path.join(ad_paths["adpath"], "scripts"),
+    "tpath" :  os.path.join(ad_paths["adpath"], "table"),
+    "upath" :  os.path.join(ad_paths["adpath"], "txt"),
+    "rpath" :  os.path.join(ad_paths["reptpath"], "pdf"),
+    "xpath" :  os.path.join(ad_paths["reptpath"], "temp"),
+    "fpath" :  os.path.join(ad_paths["reptpath"], "figures"),
+    "hpath" :  os.path.join(ad_paths["reptpath"], "html"),
+    "adbase" : ad_set["adfile"].split(".")[0]
+    }    
+    ad_files = {    
+    "ctxt" :  ".".join(["adbase"], "txt"),
+    "cpdf" :  ".".join(["adbase"], "pdf"),
+    "chtml" :  ".".join(["adbase"], "html"),
+    "trst" :  ".".join(["adbase"], "rst"),
+    "tlog" :  ".".join(["adbase"], "log"),
+    "ttex1" :  ".".join(["adbase"], "tex"),
+    "auxfile" : ".".join(["adbase"], ".aux"),
+    "outfile" :  ".".path.join(["adbase"], ".out"),
+    "texmak2" :  ".".path.join(["adbase"], ".fls"),
+    "texmak3" :  ".".path.join(["adbase"], ".fdb_latexmk")
+    }
 
-# set up files and folders
-try:
-    _rivetpath = os.path.dirname("rivet.rivet_cfg")
-    _designpath = os.path.dirname(_designfile)
-    _designname = os.path.basename(_designfile)
-    _projpath = os.path.dirname(_designpath)
-    _calcpath = os.path.join(_projpath, "calcs")
-    _bakfile = _designfile.split(".")[0] + ".bak"
-    with open(_designfile, "r") as f2:
-        _designbak = f2.read()
-    with open(_bakfile, "w") as f3:
-        f3.write(_designbak)
-    print("< folders checked and design file backup written >")
-except:
-    sys.exit("< required folders and files not found - program stopped >")
+return _path_dict, _settings
 
-# design paths
-_spath = os.path.join(_designpath, "scripts")
-_tpath = os.path.join(_designpath, "table")
-# calc paths
-_rpath = os.path.join(_calcpath, "pdf")
-_upath = os.path.join(_calcpath, "txt")
-_xpath = os.path.join(_calcpath, "temp")
-_fpath = os.path.join(_calcpath, "figures")
-_hpath = os.path.join(_calcpath, "html")
-# calc file names
-_designbase = _designname.split(".")[0]
-_ctxt = ".".join([_designbase, "txt"])
-_cpdf = ".".join([_designbase, "pdf"])
-_chtml = ".".join([_designbase, "html"])
-_trst = ".".join([_designbase, "rst"])
-_tlog = ".".join([_designbase, "log"])
-# laTex file names
-_ttex1 = ".".join([_designbase, "tex"])
-_auxfile = os.path.join(_designbase, ".aux")
-_outfile = os.path.join(_designbase, ".out")
-_texmak2 = os.path.join(_designbase, ".fls")
-_texmak3 = os.path.join(_designbase, ".fdb_latexmk")
-_cleantemp = (_auxfile, _outfile, _texmak2, _texmak3)
-# flags and variables
-_pdfflag = 0
-_nocleanflag = 0
-_verboseflag = 0
-_defaultdec = "3,3"
-_calcwidth = 80
-_calctitle = "\n\nModel Title"  # default model title
-_pdfmargin = "1.0in,0.75in,0.9in,1.0in"
+def ad_flags():
+    _cleantemp =  (ad_files["auxfile"],
+                        outfile,   
+                        texmak2, 
+                        texmak3
+                        )
+    _pdfflag = 0
+    _nocleanflag = 0
+    _verboseflag = 0
+    _defaultdec = "3,3"
+    _calcwidth = 80
+    _calctitle = "\n\nModel Title"  # default model title
+    _pdfmargin = "1.0in,0.75in,0.9in,1.0in"
+    _calcnum = _designbase[1:5]
+    _bakfile = _adfile.split(".")[0] + ".bak"
+
+_adfile = _path_dict["adfile"]
+with open(_adfile, "r") as f2:
+    _adbak = f2.read()
+with open(_bakfile, "w") as f3:
+    f3.write(_adbak)
+print("\n\nad file : ", _adfile)
+print("checks : folders checked; ad file backup written", "\n\n")
+
 
 # start checks and logs
-# check config file and folder structure and start log
 #_chk1 = chk.CheckRivet(_tlog)
 #_chk1.logstart()
 #_chk1.logwrite("< begin model processing >", 1
-
-
-# design and report settings
-with open(_designfile, 'r') as _df1:
-     _dflist = _df1.readlines()
-     for dfline in _dflist:
-         if "|[design]|" in dfline:
-             design_settings = dfline.split("|")
-         if "|[report]|" in dfline:
-             report_settings = dfline.split("|")
 
 def _string_settings(first_line):
     """prarse design string settings line
@@ -125,56 +117,73 @@ def _string_settings(first_line):
     Method called for each design string. 
     
     """
-    exec_flg = 0
+    exec_flg = -1
     sect_num = -1
     section_flg = -1
     source_flg = -1
     design_descrip = " " 
-    str_source = "" 
-    #print(first_line)
-    splt_string = first_line.split('|')
-    #print(splt_string)
-    splt_string += ["","","",""]
-    #print(splt_string)
-    i = splt_string
-    section_flg = i[1].find("]")
+    par1 = first_line
+    #print(par1)
+    section_flg = par1.find("]")
     if section_flg > -1:
-        design_descrip = i[0][section_flg:].strip()
-        sect_num = i[1][:section_flg].strip()
-        sect_num = int(sect_num.strip("]").strip("["))
-    exec_flg1 = i[1].find("[h]")
-    if exec_flg1 == -1:
-        exec_flg = 0
-    else:
-        design_descrip = i[0].replace("[h]","").strip()                
-        exec_flg = 1
-    exec_flg2 = i[1].find("[x]") 
-    if exec_flg2 == -1:
-        exec_flg = exec_flg
-    else:
-        design_descrip = i[0].replace("[x]","").strip()
+        design_descrip = par1[section_flg + 1:].strip()
+        sect_num = par1[section_flg-2:section_flg]
+        sect_num = sect_num.strip("]").strip("[")
+    if "[h]" in par1:
         exec_flg = 2
-    if len(i[2]) > 0:
-        str_source = i[2].strip()
+        design_descrip = i[0].replace("[h]","").strip()                
+    elif "[x]" in par1:
+        exec_flg = 1
+        design_descrip = i[0].replace("[x]","").strip()
     else:
-        str_source = ""
+        exec_flg = 0
 
     #print(exec_flg) 
-    return [exec_flg, sect_num, design_descrip, str_source]
+    return [exec_flg, sect_num, design_descrip]
+
+def _prt_sect(settings):
+    """Print sections to UTF-8.
+    
+    """
+    sect_num = settings[1]
+    descrip = settings[2]
+    print('=' * _calcwidth)
+    _write_utf('=' * _calcwidth, 0, 0)
+    descrip1 = " " +  descrip + (_calcnum + "-" +
+        sect_num).rjust(_calcwidth - len(descrip) - 2)
+    print(descrip1)
+    _write_utf(descrip1, 1, 0)
+    print('=' * _calcwidth)
+    _write_utf('=' * _calcwidth, 0, 0)
+
+def _write_utf(pline, pp, indent):
+    
+    """ Write model text to utf-8 encoded file.
+    
+    Args:
+        mentry (string): text 
+        pp (int): pretty print flag
+        indent (int): indent flag
+        
+    """
+    f1 = open(_ctxt, 'a')
+    if pp: pline = sy.pretty(pline, use_unicode=True, num_columns=80)
+    if indent: pline = " "*4 + pline
+    print(pline, file = f1)
+    f1.close()
 
 def r__(fstr):
     """run python code
 
     """
-    global eqnum, sectnum
     fstr1 = fstr.split("\n", 1)
     fstr2 = fstr1[1].splitlines()
     str_set = _string_settings(fstr1[0])
     if str_set[0] == 2:
         return
     if str_set[0] == 0:
-        if str_set[1] > -1:
-            print("=============================== section: " + str(str_set[1]))
+        if int(str_set[1]) > -1:
+            _prt_sect(str_set)
         for i in fstr2:
             exec(i.strip())
 
@@ -184,7 +193,7 @@ def i__(fstr):
     """evaluate insert string
     
     """
-    global eqnum, sectnum
+    global eqnum
     fstr1 = fstr.split("\n", 1)
     fstr2 = fstr1[1].splitlines()
     str_set = _string_settings(fstr1[0])
@@ -192,9 +201,9 @@ def i__(fstr):
         return
     if str_set[0] == 0:
         if int(str_set[1]) > -1:
-            print("=============================== section: " + str(str_set[1]))
+            _prt_sect(str_set)
         for i in fstr2:
-            print(i)
+            print(i.strip())
 
     global_rivet_dict.update(locals())
 
@@ -219,9 +228,12 @@ def v__(fstr):
         vdict1, vcalc1 = vcalc.vutf()
         global_rivet_dict.update(vdict1)
         if int(str_set[1]) > -1:
-            print("============================ section: " + str(str_set[1]))
+            _prt_sect(str_set)            
         for i in vcalc1:
-            print(i)
+            if "=" in i:
+                print(" "*4 + i)
+            else:
+                print(i)
 
 def e__(fstr):
     """evaluate equation string
@@ -245,10 +257,12 @@ def e__(fstr):
         edict1, ecalc1 = ecalc.eutf()
         global_rivet_dict.update(edict1)
         if int(str_set[1]) > -1:
-            print("=========================== section: " + str(str_set[1]))
+            _prt_sect(str_set)
         for i in ecalc1:
-            print(i)
-    
+            if "=" in i:
+                print(" "*4 + i)
+            else:
+                print(i)
 
 def t__(fstr):
     """evaluate table string
