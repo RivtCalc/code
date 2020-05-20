d9b990d39a0bd09df0c53162f4ee0b000a961443
rholland <rholland@structurelabs.com>
2020-05-19 22:37:24 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='r-i-v-e-t',
+    version='0.9dev',
+    packages=['rivet',],
+    license='GPLv3',
+    long_description=open('README.rst').read(),
+)
