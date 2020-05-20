415632adbfcdee9c7bc4a4183a78afba23d91e23
rholland <rholland@structurelabs.com>
2020-05-19 23:17:23 -0700

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='r-i-v-e-t',
     version='0.9dev',
-    packages=['rivet','unum'],
+    packages=['rivet','rivet.unum','rivet.unum.units','rivet.unum.units.si'],
     license='GPLv3',
     long_description=open('README.rst').read(),
 )
